0a970bcb93410bf114ed7b3e9712476f26962674
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-10-31 17:51:58 +0100

    npm init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Kluss</title>
+        <?php include_once('includes/head.inc.php'); ?>
+    </head>
+    <body>
+
+    </body>
+</html>
