e13b6df72d1ffd6e5f92de8c3cfbf8daff15a127
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-12-16 11:00:59 +0100

    Darth Laravel II

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,19 @@
-<div class="footer footer-nav-icons">
-    <div class="fp-footer">
-        <div class="fp-content">
-            <a href="#">Over ons</a>
+<footer class="footer">
+    <div class="container">
+        <div class="col-xs-1 col-sm-2">
+            <a href="#">Wat is KLUSS</a>
         </div>
-        <div class="fp-content">
+        <div class="col-xs-1 col-sm-2">
+            <a href="#">Team</a>
+        </div>
+        <div class="col-xs-1 col-sm-2">
+            <a href="#">Partners</a>
+        </div>
+        <div class="col-xs-1 col-sm-2">
             <a href="#">Privacy</a>
         </div>
+        <div class="col-xs-1 col-sm-2">
+            <a href="#">Contact</a>
+        </div>
     </div>
-</div>
+</footer>
