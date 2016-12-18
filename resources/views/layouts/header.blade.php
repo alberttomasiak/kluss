7eeaef941e02a9960e7d13ecfc13ac49b777cb40
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-12-18 14:52:23 +0100

    opmaak sass

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<div class="header">
+	<div class="left-icons"></div>
+	<div class="middle-icons"><h5>{{ $title or 'Home' }}</h5></div>
+	<div class="right-icons">
+		<a href="/profiel"><i class="icon icon-user"></i></a>
+		<a href="/logout"><i class="icon icon-share-square-o"></i></a>
+	</div>
+</div>
