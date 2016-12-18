1a3066a502469e4b5a1105dfda86eed204a06c68
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-12-18 15:33:24 +0100

    navigatie logo'tje :)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,7 @@
 <div class="header">
-	<div class="left-icons"></div>
+	<div class="left-icons">
+		<a class="kluss--logo" href="/home"></a>
+	</div>
 	<div class="middle-icons"><h5>{{ $title or 'Home' }}</h5></div>
 	<div class="right-icons">
 		<a href="/kluss_toevoegen"><i class="glyphicon glyphicon-plus"></i></a>
