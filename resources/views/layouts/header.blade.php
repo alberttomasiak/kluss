fb03b86d74d5ba63187d4a9cfe9e0ad5bf1e9ed0
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-12-18 16:23:53 +0100

    voorbereiding profiel icoontje, + laravel -> kluss naam

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,6 +4,7 @@
 	</div>
 	<div class="middle-icons"><h5>{{ $title or 'Home' }}</h5></div>
 	<div class="right-icons">
+		<a href="/profiel/{{\Auth::user()->id}}"><i class="glyphicon glyphicon-user"></i></a>
 		<a href="/kluss_toevoegen"><i class="glyphicon glyphicon-plus"></i></a>
 		<a href="/logout"><i class="glyphicon glyphicon-log-out"></i></a>
 	</div>
