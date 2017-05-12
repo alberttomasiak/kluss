d9d773aa7a6f44850fe0b3413da3b3ea84df44e1
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-12 19:31:58 +0200

    admin dashboard [WIP]

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.admin')
 @section('content')
-
+@include('admin.dashboard.partials.header')
+@include('admin.dashboard.partials.sidebar')
+<section class="dashboard--main">
+    <div class="dashboard--block">
+        <p class="registered--counter"></p>
+        <p>Geregistreerde gebruikers</p>
+    </div>
+    <div class="dashboard--block">
+        <p class="active--counter"></p>
+        <p>Actieve gebruikers</p>
+    </div>
+    <div class="dashboard--block">
+        <p class="task--counter"></p>
+        <p>Actieve klusjes</p>
+    </div>
+    <div class="gold--block">
+        <p class="gold--counter"></p>
+        <p>Gold gebruikers</p>
+    </div>
+</section>
 @endsection
