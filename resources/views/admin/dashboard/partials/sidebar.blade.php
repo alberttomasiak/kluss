d83231f6a60582b2edcdbe575711c00692d2da5c
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-12 20:02:16 +0200

    mobile styling basics

---
+++ resources/views/admin/dashboard/partials/sidebar.blade.php
@@ -0,0 +1,8 @@
+<section class="sidebar--admin">
+    <ul>
+        <li><a href="/admin/dashboard">Dashboard</a></li>
+        <li><a href="/admin/gebruikers">Gebruikers</a></li>
+        <li><a href="/admin/klusjes">Klusjes</a></li>
+        <li><a href="/admin/settings">Globale instellingen</a></li>
+    </ul>
+</section>
