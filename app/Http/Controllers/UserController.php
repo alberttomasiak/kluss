d4719871a909a854a62e0ad28a98c01ca206ea38
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-14 17:15:31 +0200

    Prepping blocked functionality #6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function login(){
-        
+    public function login(Request $request){
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])){
+            return redirect('/home');
+        }
+
+        return redirect()->back();
     }
 }
