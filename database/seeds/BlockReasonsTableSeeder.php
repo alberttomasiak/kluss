d29d4054cec268f7d24c0025923d49c4091bf68b
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-19 16:02:02 +0200

    BlockReasons table seed

--- database/seeds/BlockReasonsTableSeeder.php
+++ database/seeds/BlockReasonsTableSeeder.php
@@ -11,6 +11,19 @@ class BlockReasonsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('block_reasons')->insert([
+            'name' => 'Misbruik regels'
+        ]);
+        DB::table('block_reasons')->insert([
+            'name' => 'Grof gedrag t.o.v. anderen'
+        ]);
+        DB::table('block_reasons')->insert([
+            'name' => 'Plaatsing van offensieve klusjes'
+        ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
