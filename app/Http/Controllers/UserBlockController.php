7823d95c3c8a2bec7e67a6e3d45144f1dac8f7f7
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-19 15:11:49 +0200

    User block Controller

---
+++ app/Http/Controllers/UserBlockController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserBlockController extends Controller
+{
+    public function blockUser(Request $request){
+        dd($request);
+    }
+}
