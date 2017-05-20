ef18863ada43ec8a16110976dbef28e777bb49ea
SamuelSieben <r0486707@student.thomasmore.be>
2017-05-20 16:09:47 +0200

    Commit 2: Fonts, Design, Pagina's
    commit 2

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -26,13 +26,21 @@
 
 <div class="addboxshadow container" style="height: 700px;">
     <h1 style="float: left; padding: 15px;">Algemene Voorwaarden</h1>
-    <hr style="background-color: #4C4C4B; height: 1px; width: 100%; opacity: 0.3; ">
-    <div style="float: left; width: 20%; border-right: 1px solid #4C4C4B;">
+    <hr style="background-color: #677578; height: 0.2px; width: 100%; ">
+    <div style="height: 605px; float: left; width: 20%; margin-left: 30px; margin-top: -10px; border-right: 1px solid #677578;">
+        <section style="margin-top: 30px;">
         <a href="#">Disclaimer</a>
         <br><br>
         <a href="#">Terug</a>
+            </section>
         </div>
-    <div style="float: left; width: 80%">
+    <div style="float: right; width: 60%; margin-right: 50px; color: #677578;">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit enim, eleifend non finibus convallis, hendrerit ut diam. Praesent accumsan facilisis elit, ut volutpat lectus. Nulla vestibulum lobortis efficitur. Aliquam ut imperdiet turpis, vel consequat nibh. Phasellus sed eleifend turpis. Pellentesque luctus elementum magna, vitae placerat odio consectetur ut. Nam vehicula lacinia arcu. Donec porttitor odio leo, rhoncus tempus mi dictum non.
+
+            Etiam posuere ex in magna laoreet, vitae malesuada quam ultricies. Duis dignissim scelerisque mollis. Nam dapibus aliquet leo, ut bibendum orci consectetur et. Suspendisse potenti. Integer ultricies tellus dui, in sodales neque molestie ac. Morbi blandit, nisi in cursus vestibulum, lectus erat commodo nisi, sed molestie augue erat et nibh. Morbi ornare ultrices tellus, sed efficitur magna aliquet eu. Praesent faucibus magna sit amet augue egestas, quis porta leo suscipit. Aenean tincidunt, purus at dignissim sodales, tellus lectus rutrum leo, eu consectetur metus nulla non neque. Nulla blandit eleifend vehicula. Curabitur ornare magna sed augue laoreet vulputate. Quisque elementum, velit vel lacinia fringilla, orci tortor suscipit purus, id dignissim ligula justo vel ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec placerat, mi nec pulvinar venenatis, turpis lorem rhoncus felis, non aliquet ligula leo sed mi. Suspendisse porta massa eleifend, euismod velit id, laoreet sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+            Sed vel, tortor eget aliquam porttitor, tortor diam aliquam ante, eget bibendum orci metus ut nunc. Maecenas nec sollicitudin felis. Donec vel luctus leo, et consectetur magna. Maecenas faucibus scelerisque nisi, ullamcorper ultrices quam sagittis id. Ut varius lectus ut vestibulum ullamcorper. Nullam vitae nunc metus.
+        </p>
         </div>
 
 </div>
@@ -45,19 +53,19 @@
         <section>
             <p>KLUSS</p>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Algemene Voorwaarden</a></li>
-                <li><a href="#">FAQ</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">Home</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">Team</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">Algemene Voorwaarden</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">FAQ</a></li>
 
             </ul>
         </section>
         <section style="margin-right:0px !important;">
             <p>CONTACT</p>
             <ul>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">Twitter</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">Contact</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">Facebook</a></li>
+                <li><a style="color: #FFFFFF !important;" href="#">Twitter</a></li>
 
             </ul>
 
