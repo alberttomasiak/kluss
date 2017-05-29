d5648aa36f02b3c80f3cf29f96432b4908f04333
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-29 21:26:19 +0200

    settings modal

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.admin')
 @section('content')
-
+    <div class="users--overview">
+        <h1>Globale Instellingen</h1>
+        <div class="settings-outer">
+        <h2>Overzicht</h2>
+        <a href="#addSettingModal" data-toggle="modal" role="button" class="addSettingModal add--setting-btn">+</a>
+        @include('admin.settings.modals.add')
+        <div class="settings-overview">
+            <div class="settings-titles">
+                <h6>Key</h6>
+                <h6>Value</h6>
+            </div>
+            @foreach($settings as $setting)
+                <div class="settings-div">
+                    <div class="settings-key">
+                        {{$setting->key}}
+                    </div>
+                    <div class="settings-value">
+                        {{$setting->value}}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        {{$settings->links()}}
+        </div>
+    </div>
 @endsection
