81a9eb1de4a6f7960d67abfdb12753a0dd33d7b2
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-05-30 12:32:14 +0200

    Global settings seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(klussSeeder::class);
         $this->call(BlockReasonsTableSeeder::class);
+        $this->call(GlobalSettingsTableSeeder::class);
     }
 }
