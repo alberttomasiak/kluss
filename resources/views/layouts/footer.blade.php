122b854d36e6c6093445a481c6e3c0dabf8ab144
thomas van malderen <thomas.vanmalderen@student.thomasmore.be>
2017-06-16 15:46:16 +0000

    Styling static pages

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,13 @@
 <footer class="main--footer">
     <div class="footer_list">
-        <a href="www.google.com" class="landing_footer_link">KLUSS</a>
-        <a href="www.facebook.com" class="landing_footer_link">Team</a>
-        <a href="www.twitter.com" class="landing_footer_link">Algemene Voorwaarden</a>
-        <a href="www.instagram.com" class="landing_footer_link">FAQ</a>
-        <a href="www.weareimd.be" class="landing_footer_link">Contact</a>
-        <a href="www.weareimd.be" class="landing_footer_link">Kluss Gold</a>
-        <a href="www.weareimd.be" class="landing_footer_link">Facebook</a>
-        <a href="www.weareimd.be" class="landing_footer_link">Twitter</a>
+        <a href="/" class="landing_footer_link">KLUSS</a>
+        <a href="/team" class="landing_footer_link">Team</a>
+        <a href="/terms" class="landing_footer_link">Algemene Voorwaarden</a>
+        <a href="/FAQ" class="landing_footer_link">FAQ</a>
+        <a href="/contact" class="landing_footer_link">Contact</a>
+        <a href="/klussgold" class="landing_footer_link">Kluss Gold</a>
+        <a href="https://www.facebook.com/klussapp/" class="landing_footer_link">Facebook</a>
+        <a href="https://twitter.com/Klussapp" class="landing_footer_link">Twitter</a>
     </div>
     <div class="extra--info">
         <a href="mailto:hi@kluss.be">hi@kluss.be</a>
