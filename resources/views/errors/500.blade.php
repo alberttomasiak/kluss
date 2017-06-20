822c2cc3a16ca7e357e63f76d3c4aa73d9b30ead
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-06-20 14:03:51 +0200

    500 error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,7 +5,7 @@
         <div class="not-found-main">
             <img src="/assets/img/error.png" alt="500 Error">
             <h2>Er is iets misgelopen!</h2>
-            <p>Klik <a href="/">hier</a> om terug naar de homepagina van kluss.be te gaan</p>
+            <p>Klik <a href="{{ url()->previous() }}">hier</a> om terug te gaan naar de vorige pagina.</p>
         </div>
     </div>
 @endsection
