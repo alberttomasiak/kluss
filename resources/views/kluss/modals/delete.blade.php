74d8a03839a7732b0d07bba6c3cc3e7667231b67
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-06-20 17:11:04 +0200

    delete-modal fix

--- resources/views/kluss/modals/delete.blade.php
+++ resources/views/kluss/modals/delete.blade.php
@@ -6,14 +6,14 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h3 class="modal-title">Klusje "{{$kl->title}}" verwijderen</h3>
       <div class="modal-body">
-          <form action="/kluss/{{$kl->id}}/verwijderen" id="kluss-{{$kl->id}}-verwijderen" method="post">
+          <form action="/kluss/{{$kl->id}}/verwijderen" name="kluss-verwijderen" id="kluss-verwijderen" method="post">
               {!! csrf_field() !!}
               <p>Ben je zeker dat je het klusje wil verwijderen?</p>
               <input type="hidden" name="blocker_id" id="blocker_id" value="{{\Auth::user()->id}}">
       </div>
       <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Annuleren</button>
-        <button type="submit" form="kluss-{{$kl->id}}-verwijderen" class="btn btn-danger">Verwijderen</button>
+        <button type="submit" form="kluss-verwijderen" class="btn btn-danger">Verwijderen</button>
         </form>
       </div>
     </div>
